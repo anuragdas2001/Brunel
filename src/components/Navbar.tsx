8b255d76e3787ad219ad162bb191dff39b0e6747
Anurag Das <anuragdas2001>
2024-05-10 19:50:17 +0530

    Final Commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,10 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
 
 export const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // const [showCloseButton, setShowCloseButton] = useState(false);
 
   const handleProjects = () => {
     console.log("HandleProjects");
@@ -14,16 +12,15 @@ export const Navbar = () => {
     // setShowCloseButton(true);
   };
 
-  const handleClose = () => {
-    navigate("/");
-    // setShowCloseButton(false);
-  };
-
   return (
     <>
       <div className="border-2 border-white mt-3">
         <div className=" float-left  ms-3">
-          <img className=" m-0 h-14 w-40 p-1" src="/Frame 1261155216.png" alt="" />
+          <img
+            className=" m-0 h-14 w-40 p-1"
+            src="/Frame 1261155216.png"
+            alt=""
+          />
         </div>
         <div className="float-right m-0">
           {location.pathname === "/" && (
@@ -39,14 +36,7 @@ export const Navbar = () => {
               </button>
             </>
           )}
-          {/* {location.pathname === "/registration" && showCloseButton && (
-            <button
-              className="rounded-full text-xs border-2 me-3 p-4"
-              onClick={handleClose}
-            >
-              <img src="/Frame 1261155540.png" alt="" />
-            </button>
-          )} */}
+          
         </div>
       </div>
       <Outlet />
