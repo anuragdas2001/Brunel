7d0b2603f8fc0dd202a1b24ebc5e6b2930c3a8ce
Anurag Das <anuragdas2001>
2024-05-16 20:37:28 +0530

    Staging...

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -5,16 +5,16 @@ export const Explore = () => {
         <div className="absolute">
           <h1 className="text-3xl">Primary CTA</h1>
           <br />
-          <div className=" mt-0">
+          <div className="-ms-10   mt-0">
             <button className="absolute h-16 w-64 p-1 bg-gray-950 text-white rounded-full hover:bg-[#4E4E4E]">
               Meet Your Candidates →
             </button>
           </div>
         </div>
-        <div className="absolute  mt-72">
+        <div className="absolute  mt-80">
           <h1 className="text-3xl">Secondary CTA</h1>
           <br />
-          <button className="absolute  h-16 w-40 p-1 border-2 bg-gray-50 text-black rounded-full hover:bg-[#F1F1F1] ">
+          <button className="absolute ms-4 h-16 w-40 p-1 border-2 bg-gray-50 text-black rounded-full hover:bg-[#F1F1F1] ">
             Get Projects
           </button>
         </div>
