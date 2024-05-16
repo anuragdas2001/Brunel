a54551fcf0fd8996ed511281728c740db3da5f82
Anurag Das <anuragdas2001>
2024-05-16 20:30:49 +0530

    Staging...

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -3,7 +3,7 @@ export const Explore = () => {
     <>
       <div className="flex justify-center items-center align-middle mt-10 ">
         <div className="absolute">
-          <h1 className="text-base">Primary CTA</h1>
+          <h1 className="text-3xl">Primary CTA</h1>
           <br />
           <div className=" mt-0">
             <button className="absolute -ml-20 h-16 w-64 p-1 bg-gray-950 text-white rounded-full hover:bg-[#4E4E4E]">
@@ -12,9 +12,9 @@ export const Explore = () => {
           </div>
         </div>
         <div className="absolute  mt-72">
-          <h1 className="text-base">Secondary CTA</h1>
+          <h1 className="text-3xl">Secondary CTA</h1>
           <br />
-          <button className="absolute -ml-5 h-20 w-40 p-1 border-2 bg-gray-50 text-black rounded-full hover:bg-[#F1F1F1] ">
+          <button className="absolute -ml-5 h-16 w-40 p-1 border-2 bg-gray-50 text-black rounded-full hover:bg-[#F1F1F1] ">
             Get Projects
           </button>
         </div>
